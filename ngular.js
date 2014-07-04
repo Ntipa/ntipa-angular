034bf8c2d229d53403b642e5c32d41e88ace54a0
tindaro tornabene <tindaro.tornabene@ietservizi.it>
2014-07-04 17:12:04 +0200

    add

--- ngular.js
+++ ngular.js
@@ -13,9 +13,7 @@ angular.module('ipublic.ntipa-angular', [])
     function ($resource) {
         return $resource('./app/rest/account', {}, {
         });
-    }]);
-
-angular.module('ipublic.ntipa-angular', [])
+    }])
 .factory('Ente', ['$resource',
    function ($resource) {
        return $resource('/manager/app/rest/account/:action/:enteId/:gruppoId', {}, {
@@ -23,27 +21,221 @@ angular.module('ipublic.ntipa-angular', [])
            'organigramma': { method: 'GET', isArray: false, params: { 'action': 'enties','gruppoId':'organigramma'}},
            'group': { method: 'GET', isArray: false, params: { 'action': 'groups'}}
        });
-   }]);
-
-angular.module('ipublic.ntipa-angular', [])
+   }])
 .factory('Password', ['$resource',
     function ($resource) {
         return $resource('/manager/app/rest/account/change_password', {}, {
         });
-    }]);
-
-
-angular.module('ipublic.ntipa-angular', [])
+    }])
 .factory('Sessions', ['$resource',
     function ($resource) {
         return $resource('/manager/app/rest/account/sessions/:series', {}, {
             'get': { method: 'GET', isArray: true}
         });
-    }]);
+    }])
+.factory('Oauth2Service', ['$rootScope', '$http', 'authService', 'Session', 'Account',  '$log', 'localStorageService','Ente','$location','$base64',
+    function ($rootScope, $http, authService, Session, Account,$log, localStorageService, Ente,$location,$base64) {
+        var keyAuthorization = 'Authorization';
+        var keyAccessToken = 'access.token';
+        var keySession = 'user.session';
+        var keyEntiesSession = 'enties.session';
+        var keyOrganigramma = 'organigramma';
+        
+        var keyCategorias = 'keyCategorias';
+        var keyTitolaris = 'keyTitolaris';
+        var keyStrutturas = 'keyStrutturas';
+        var keyUsers = 'keyUsers';
+
+        var keyMapCategorias = 'keyMapCategorias';
+        var keyMapTitolaris = 'keyMapTitolaris';
+     
 
 
 
-angular.module('ipublic.ntipa-angular', [])
+        function recursiveVociTitolario(nodes,titolarioName,titolarioId){
+
+            angular.forEach(nodes, function(nodo) {
+                nodo.titolarioId = titolarioId;
+                nodo.titolarioName = titolarioName ;
+
+                var idVoce = nodo.titolarioId+"|"+nodo.id;
+
+                //var idBase64Voce = $base64.encode(idVoce);
+                
+                nodo.idVoce = idVoce ;
+
+                $rootScope.titolari.push(nodo);
+
+                recursiveVociTitolario(nodo.nodes,nodo.titolarioName , titolarioId);
+            });
+        };
+
+        function recursiveStruttura(struttura,paths){
+            $log.debug('struttura:'+struttura);
+            var paths = paths+ ' ' +struttura.name;
+
+            angular.forEach(struttura.categorie, function(categoria) {
+             $rootScope.categorias.push(categoria);
+             });
+
+              angular.forEach(struttura.titolari, function(titolario) {
+                titolario.nodes = angular.fromJson(titolario.voci);
+                titolario.voci = null;
+
+                recursiveVociTitolario(titolario.nodes,titolario.name,titolario.id);
+                
+             });
+
+
+            angular.forEach(struttura.gruppi, function(gruppo) {
+             gruppo.struttura= {name:paths};
+             $rootScope.strutturas.push(gruppo);
+
+             angular.forEach(gruppo.utenti, function(utente) {
+                 $rootScope.users.push(utente);
+             });
+
+         });
+
+            angular.forEach(struttura.children, function(struttura2) {
+              recursiveStruttura(struttura2,paths);
+          });
+
+        };
+
+        return {
+            loadAccount:  function (){
+                Account.get(function(data) {
+                    
+
+                    $rootScope.account = data;
+                    localStorageService.add(keySession, data);
+                    $log.debug('data.enteId:'+data.enteId);
+                    
+                    if(data.enteId != null && data.enteId != 'null' ){
+                        Ente.organigramma({enteId:data.enteId},function(data){
+                            $rootScope.organigramma = data;
+                            localStorageService.add(keyOrganigramma, $rootScope.organigramma);
+
+                            $rootScope.users = [];
+                            $rootScope.categorias = [];
+                            $rootScope.titolari = [];
+                            $rootScope.strutturas = [];
+
+                            $log.debug(data);
+                            angular.forEach(data.strutture, function(struttura) {
+                                recursiveStruttura(struttura, '' );
+                            } );
+
+
+                            localStorageService.remove(keyCategorias );
+                            localStorageService.remove(keyStrutturas );
+                            localStorageService.remove(keyTitolaris );
+                            localStorageService.remove(keyUsers );
+
+                            localStorageService.add(keyCategorias, $rootScope.categorias);
+                            localStorageService.add(keyStrutturas, $rootScope.strutturas);
+                            localStorageService.add(keyTitolaris, $rootScope.titolari);
+                            localStorageService.add(keyUsers, $rootScope.users);
+
+
+                            $rootScope.mapCategorias = {};
+                            angular.forEach($rootScope.categorias , function( categoria  ) {
+                              $rootScope.mapCategorias[categoria.id] = categoria;
+                            } );
+                            localStorageService.add(keyMapCategorias, $rootScope.mapCategorias);
+                           
+
+                            $rootScope.mapTitolaris = {};
+                            angular.forEach($rootScope.titolari , function( item  ) {
+                              $rootScope.mapTitolaris[item.idVoce] = item;
+                            } );
+                            localStorageService.add(keyMapTitolaris, $rootScope.mapTitolaris);
+
+                        });
+                }
+
+                Ente.enties(function(data){
+                 localStorageService.add(keyEntiesSession, data );
+                 $rootScope.enties = data;
+                });
+
+                Session.create(data.login, data.firstName, data.lastName, data.email, data.roles);
+                authService.loginConfirmed(data);
+                });
+            },
+            loadLocalToken: function() {
+              $rootScope.token = localStorageService.get(keyAuthorization);
+              $rootScope.accessToken = localStorageService.get(keyAccessToken);
+              $log.info('getLocalToken:' + $rootScope.token);
+              if ($rootScope.token !== null) {
+                $http.defaults.headers.common[keyAuthorization] = $rootScope.token;
+
+                var data = localStorageService.get(keySession);
+                $rootScope.enties =localStorageService.get(keyEntiesSession );
+
+                if(data != null && data.login != null){
+                    $rootScope.account = data;
+                    Session.create(data.login, data.firstName, data.lastName, data.email, data.roles);
+                    $rootScope.users = [];
+                    $rootScope.strutturas = [];
+                    $rootScope.categorias = [];
+                    $rootScope.titolari = [];
+
+                    $rootScope.organigramma = localStorageService.get(keyOrganigramma );
+                    $rootScope.categorias = localStorageService.get(keyCategorias);
+                    $rootScope.strutturas = localStorageService.get(keyStrutturas);
+                    $rootScope.titolari = localStorageService.get(keyTitolaris);
+                    $rootScope.users = localStorageService.get(keyUsers);
+
+                    $rootScope.mapCategorias =    localStorageService.get(keyMapCategorias );
+                    $rootScope.mapTitolaris =    localStorageService.get(keyMapTitolaris );
+
+
+                    $log.info('getLocaluser:' );
+                    $log.info($rootScope.account);
+                }else{
+                   $location.path('/login').replace();
+               }
+
+            }
+            },clear: function () {
+                $rootScope.authenticationError = false;
+                $rootScope.authenticationError = false;
+                $rootScope.authenticated = false;
+
+                $log.info('clear');
+                $rootScope.username = '';
+                $rootScope.password = '';
+                $rootScope.account = null;
+                $rootScope.user = null;
+               
+                $rootScope.token = '';
+                $rootScope.accessToken = '';
+
+                localStorageService.remove(keyAuthorization);
+                localStorageService.remove(keyAccessToken);
+                localStorageService.remove(keySession);
+
+                
+               localStorageService.remove(keyEntiesSession);
+               localStorageService.remove(keyCategorias);
+               localStorageService.remove(keyTitolaris);
+               localStorageService.remove(keyStrutturas);
+               localStorageService.remove(keyUsers);
+            
+
+               Session.invalidate();
+
+              delete $http.defaults.headers.common[keyAuthorization];
+          
+
+        }
+
+
+
+            };
+}])
 .factory('AuthenticationSharedService', ['$rootScope', '$http', 'authService', 'Session', 'Account',  '$log', 'localStorageService','Ente','Oauth2Service',
 	function ($rootScope, $http, authService, Session, Account,$log, localStorageService, Ente, Oauth2Service) {
 		var keyAuthorization = 'Authorization';
